29a918486a45d5e00cdaa8c2b77435b11e413bcf
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-16 22:59:57 +0430

    update

--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import $ from 'jquery';
-import Popper from 'popper.js';
+// import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle';
 
 
