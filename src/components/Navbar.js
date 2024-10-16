6b561f7a1aaaf5f2c87fa8b0a8732c4ace324a2a
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-17 00:47:16 +0430

    update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,14 +9,14 @@ export default function Navbar() {
       <nav id="navbar">
         <ul className="navbar-items flexbox-col">
           <li className="navbar-logo flexbox-left">
-            <a className="navbar-item-inner flexbox">
-            <i className="fa-regular fa-brightness"></i>
+            <a className="navbar-item-inner flexbox active">
+            <i className="fa-thin fa-brightness"></i>
             </a>
           </li>
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="search-outline" />
+              <i className="fa-thin fa-magnifying-glass"></i>
               </div>
               <span className="link-text">Search</span>
             </a>
@@ -24,7 +24,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="home-outline" />
+              <i className="fa-thin fa-home"></i>
               </div>
               <span className="link-text">Home</span>
             </a>
@@ -32,7 +32,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="folder-open-outline" />
+              <i className="fa-thin fa-folders"></i>
               </div>
               <span className="link-text">Projects</span>
             </a>
@@ -40,7 +40,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="pie-chart-outline" />
+              <i className="fa-thin fa-chart-pie"></i>
               </div>
               <span className="link-text">Dashboard</span>
             </a>
@@ -48,7 +48,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="people-outline" />
+              <i className="fa-thin fa-users"></i>
               </div>
               <span className="link-text">Team</span>
             </a>
@@ -56,7 +56,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="chatbubbles-outline" />
+              <i className="fa-thin fa-messages"></i>
               </div>
               <span className="link-text">Support</span>
             </a>
@@ -64,7 +64,7 @@ export default function Navbar() {
           <li className="navbar-item flexbox-left">
             <a className="navbar-item-inner flexbox-left">
               <div className="navbar-item-inner-icon-wrapper flexbox">
-                <ion-icon name="settings-outline" />
+              <i className="fa-thin fa-gear"></i>
               </div>
               <span className="link-text">Settings</span>
             </a>
@@ -73,15 +73,6 @@ export default function Navbar() {
       </nav>
       {/* Main */}
       <main id="main" className="flexbox-col">
-        <h2>Lorem ipsum!</h2>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
-          corporis, rerum doloremque iste sed voluptates omnis molestias
-          molestiae animi recusandae labore sit amet delectus ad necessitatibus
-          laudantium qui! Magni quisquam illum quaerat necessitatibus sint
-          quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis
-          iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?
-        </p>
       </main>
     </>
   );
