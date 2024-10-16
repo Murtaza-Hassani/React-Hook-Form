a8370659ad18275f43034c048bfe93c4ed132715
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-16 22:51:10 +0430

    update

---
+++ src/components/Navbar.js
@@ -0,0 +1,93 @@
+import React from 'react'
+import './Navbar.css';
+export default function Navbar() {
+  return (
+    <>
+      <nav id="navbar">
+        <ul className="navbar-items flexbox-col">
+          <li className="navbar-logo flexbox-left">
+            <a className="navbar-item-inner flexbox">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                id="Layer_1"
+                data-name="Layer 1"
+                viewBox="0 0 1438.88 1819.54"
+              >
+                <polygon points="925.79 318.48 830.56 0 183.51 1384.12 510.41 1178.46 925.79 318.48" />
+                <polygon points="1438.88 1663.28 1126.35 948.08 111.98 1586.26 0 1819.54 1020.91 1250.57 1123.78 1471.02 783.64 1663.28 1438.88 1663.28" />
+              </svg>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="search-outline" />
+              </div>
+              <span className="link-text">Search</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="home-outline" />
+              </div>
+              <span className="link-text">Home</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="folder-open-outline" />
+              </div>
+              <span className="link-text">Projects</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="pie-chart-outline" />
+              </div>
+              <span className="link-text">Dashboard</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="people-outline" />
+              </div>
+              <span className="link-text">Team</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="chatbubbles-outline" />
+              </div>
+              <span className="link-text">Support</span>
+            </a>
+          </li>
+          <li className="navbar-item flexbox-left">
+            <a className="navbar-item-inner flexbox-left">
+              <div className="navbar-item-inner-icon-wrapper flexbox">
+                <ion-icon name="settings-outline" />
+              </div>
+              <span className="link-text">Settings</span>
+            </a>
+          </li>
+        </ul>
+      </nav>
+      {/* Main */}
+      <main id="main" className="flexbox-col">
+        <h2>Lorem ipsum!</h2>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum
+          corporis, rerum doloremque iste sed voluptates omnis molestias
+          molestiae animi recusandae labore sit amet delectus ad necessitatibus
+          laudantium qui! Magni quisquam illum quaerat necessitatibus sint
+          quibusdam perferendis! Aut ipsam cumque deleniti error perspiciatis
+          iusto accusamus consequuntur assumenda. Obcaecati minima sed natus?
+        </p>
+      </main>
+    </>
+  );
+}
