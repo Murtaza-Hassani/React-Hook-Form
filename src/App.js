cd3a9e5a8f74450cc11d8c5f1db0d7670f17f396
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-17 00:12:51 +0430

    update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Navbar from './components/Navbar'
 import './App.css';
 import './components/Navbar.css';
+import './components/all.css';
+import './components/all.min.css';
 
 const App = () => {
   return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from 'react'
 import './Navbar.css';
+import './all.css';
+import './all.min.css';
 export default function Navbar() {
   return (
     <>
@@ -8,15 +10,7 @@ export default function Navbar() {
         <ul className="navbar-items flexbox-col">
           <li className="navbar-logo flexbox-left">
             <a className="navbar-item-inner flexbox">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                id="Layer_1"
-                data-name="Layer 1"
-                viewBox="0 0 1438.88 1819.54"
-              >
-                <polygon points="925.79 318.48 830.56 0 183.51 1384.12 510.41 1178.46 925.79 318.48" />
-                <polygon points="1438.88 1663.28 1126.35 948.08 111.98 1586.26 0 1819.54 1020.91 1250.57 1123.78 1471.02 783.64 1663.28 1438.88 1663.28" />
-              </svg>
+            <i className="fa-regular fa-brightness"></i>
             </a>
           </li>
           <li className="navbar-item flexbox-left">
