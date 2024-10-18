794ada69ab9c4f4663a148ff64d1035dd250c7bc
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-18 22:02:53 +0430

    update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
-import React from 'react'
-import Navbar from './components/Navbar'
+import logo from './logo.svg';
 import './App.css';
-import './components/Navbar.css';
-import './components/all.css';
-import './components/all.min.css';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Navbar/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './../node_modules/bootstrap/dist/css/bootstrap.min.css';
-// import Popper from 'popper.js';
-import 'bootstrap/dist/js/bootstrap.bundle';
-
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
