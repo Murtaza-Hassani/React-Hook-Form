053d2f728045c6508229bcd6745d2daaac1732aa
Murtaza-Hassani <murtazahassani7@gmail.com>
2024-10-20 18:35:50 +0430

    update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './Pages/Navbar';
 import Home from './Pages/Home';
 import Projects from './Pages/Projects';
@@ -10,21 +10,21 @@ import Contact from './Pages/Contact';
 
 
 function Locations() {
-  return(
-      <Routes>
-          <Route path='/Home' element={<Home/>}/>
-          <Route path='/Projects' element={<Projects/>}/>
-          <Route path='/Dashboard' element={<Dashboard/>}/>
-          <Route path='/Teams' element={<Teams/>}/>
-          <Route path='/Contact' element={<Contact/>}/>
-      </Routes>
+  return (
+    <Routes>
+      <Route path='/Home' element={<Home />} />
+      <Route path='/Projects' element={<Projects />} />
+      <Route path='/Dashboard' element={<Dashboard />} />
+      <Route path='/Teams' element={<Teams />} />
+      <Route path='/Contact' element={<Contact />} />
+    </Routes>
   );
 }
 
 function App() {
   return (
     <Router>
-      <Navbar location ={<Locations/>} />
+      <Navbar location={<Locations />} />
     </Router>
   );
 }
